0b60022d0879dc485a96ef300504528b465f053a
Koma4k <santadiscord2@gmail.com>
2025-05-05 17:47:37 -0400

    RoReveal 1.0

---
+++ RoReveal.lua
@@ -0,0 +1,24 @@
+--[[
+    RoReveal Script for Roblox
+    Features:
+    - Box ESP
+    - Name ESP
+    - Tracers
+    - Health Bars
+    - Distance Tags
+    - Team Check
+    - Visibility Toggle
+    - Rainbow Effect
+    - Nearest Player Indicator
+    - Full Customization
+    - Change Toggle Key
+    - Unload Script
+
+    - Discord: https://discord.gg/koma4k
+    - Website: https://koma4k.xyz/
+    - Version: 1.0 - Initial Release
+    - Author: Koma4k
+    - Last Updated: 5/5/2025
+    - Credits: Orion UI Library (https://github.com/jensonhirst/Orion)
+]]
+loadstring(game:HttpGet("https://cdn.koma4k.xyz/download/RoReveal_1.0.lua"))()
