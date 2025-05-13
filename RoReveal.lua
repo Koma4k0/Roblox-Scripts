acbe82835458d12f043329b77991fc8b1939c945
Koma4k <147834010+Koma4k0@users.noreply.github.com>
2025-05-13 11:38:41 -0400

    Added Future Features List

--- RoReveal.lua
+++ RoReveal.lua
@@ -20,5 +20,14 @@
     - Author: Koma4k
     - Last Updated: 5/5/2025
     - Credits: Orion UI Library (https://github.com/jensonhirst/Orion)
+
+    Features to be added in RoReveal V2 (Update Expected 5/18/25):
+    - Config Saving & Loading
+    - New Hot Keys Tab (set hotkeys for all features)
+    - 3D ESP
+    - Player Outline ESP
+    - Skeleton ESP
+    - Add Health % to Health ESP
+    
 ]]
 loadstring(game:HttpGet("https://cdn.koma4k.xyz/download/loader_v2.lua"))()
