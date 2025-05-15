c04fca302c2a5e0da1b5aef5789465c8001cdfdd
Koma4k <santadiscord2@gmail.com>
2025-05-15 15:28:42 -0400

    Updated RoReveal to version 2.0

--- RoReveal.lua
+++ RoReveal.lua
@@ -1,7 +1,10 @@
 --[[
     RoReveal Script for Roblox
     Features:
-    - Box ESP
+    - 2D Box ESP
+    - 3D Box ESP
+    - Skeleton ESP
+    - Outline ESP
     - Name ESP
     - Tracers
     - Health Bars
@@ -11,23 +14,24 @@
     - Rainbow Effect
     - Nearest Player Indicator
     - Full Customization
-    - Change Toggle Key
+    - Keybind for ESP Toggle (Soon Keybinds for all features) 
     - Unload Script
 
     - Discord: https://discord.gg/koma4k
     - Website: https://koma4k.xyz/
-    - Version: 1.0 - Initial Release
+    - Version: 2.0
     - Author: Koma4k
-    - Last Updated: 5/5/2025
+    - Last Updated: 5/15/2025
     - Credits: Orion UI Library (https://github.com/jensonhirst/Orion)
 
-    Features to be added in RoReveal V2 (Update Expected 5/18/25):
-    - Config Saving & Loading
-    - New Hot Keys Tab (set hotkeys for all features)
-    - 3D ESP
-    - Player Outline ESP
-    - Skeleton ESP
+    Features to be added in RoReveal V3:
     - Add Health % to Health ESP
+    - Add Radar ESP
+    - Add Chams ESP
+    - Add Per Player ESP (Select which player to enable/disable)
+    - Add NPC ESP (Enable/Disable)
+    - Add Direction Arrows (Enable/Disable)
     
+
 ]]
 loadstring(game:HttpGet("https://cdn.koma4k.xyz/download/loader_v2.lua"))()
